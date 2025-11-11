f27828bfb4feb4e7e6cf7fecff3d345dc921a919
KendrickB-S <kendrik.23.bs@gmail.com>
2025-11-11 01:20:59 -0500

    usuario repository modificado

--- src/main/java/com/example/bibliotecaproyecto/repository/UsuarioRepository.java
+++ src/main/java/com/example/bibliotecaproyecto/repository/UsuarioRepository.java
@@ -4,6 +4,10 @@ import com.example.bibliotecaproyecto.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional; // Importar
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    // Spring Data JPA crea la consulta automáticamente
+    Optional<Usuario> findByEmail(String email);
 }
